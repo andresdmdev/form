b4d307bb07dc1d5d1e834f21adada1fad0974f6b
Andres Marquez <andresdmf55@gmail.com>
2022-07-11 22:12:00 -0500

    Change styles and some details

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import Form from './form/Form';
+
+function App() {
+  return (
+    <>
+      <div className='App_Form'>
+        <Form />
+      </div>
+    </>
+  );
+}
+
+export default App;
