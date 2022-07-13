99322bc35a4c2fd92be51e29fc19ecaa211c4f21
Andres Marquez <andresdmf55@gmail.com>
2022-07-13 13:35:01 -0500

    Fixed address input

--- src/components/form/address/Address.js
+++ src/components/form/address/Address.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../../store/slices/userSlice";
 
-export default function Address({ address, handleAddress }){
+export default function Address(){
+
+  const { address } = useSelector(state => state.userSlice)
+
+  const dispatch = useDispatch()
+
+  const [count, setCount] = useState(0)
+
+  function handleAddress(event) {
+
+    const { name, value } = event.target
+
+    if (value.length <= 250) {
+
+      dispatch(setUser({ name, value }))
+
+      setCount(value.length)
+    } else {
+      setCount(value.length - 1)
+    }
+  }
+
   return (
     <div className="form_field">
       <label htmlFor="address" className="form_field_label">Address</label>
@@ -12,6 +36,7 @@ export default function Address({ address, handleAddress }){
         placeholder='Address here...'
         className="form_field_textarea"
       />
+      <p className="form_field_counter">{count}/250</p>
     </div>
   )
 }
