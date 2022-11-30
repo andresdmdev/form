66bc6b479f5eb9c0bd117d81fbf3ff1249c737fb
Andres Marquez <andresdmf55@gmail.com>
2022-11-29 22:42:49 -0500

    Changed Small card component

--- src/components/cards/SmallCard.js
+++ src/components/cards/SmallCard.js
@@ -1,28 +1,35 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setCard } from "../store/slices/cardSlice";
+import React, { useContext } from "react";
+import UserContext from "../../context/UserContext";
 import './styles/smallCard.css'
 
-export default function SmallCard(){
+export default function SmallCard({ showCard }){
 
-  const { firstName, lastName, email } = useSelector(state => state.userSlice)
-
-  const dispatchCard = useDispatch()
-
-  function handleClick(){
-    
-    dispatchCard(setCard({ name: 'bigCard', value: true }))
-  }
+  const { userData } = useContext(UserContext)
 
   return (
-    <div className="card_small" id="smallCard">
-      <div className="card_small_info">
-        <h2 className="card_small_greeting">Hello 🎉</h2>
-        <h3 className="card_small_name">{firstName} {lastName}</h3>
-        <p className="card_small_msg">
-          We'll send an email to {email} to confirm your subscription
-        </p>
-        <button className='card_small_btn' onClick={handleClick}>Show info</button>
+    <div className="fixed top-32 left-1/10 z-50 bg-600 text-50 w-4/5 py-7 px-7 rounded-3xl" id="smallCard" data-testid='smallCard'>
+      <h2 className="text-4xl font-semibold mb-6">Hello 🎉</h2>
+      <h3 className="text-2xl font-semibold mb-2">{userData.firstName} {userData.lastName}</h3>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="bg-300 mb-3 w-full rounded-md px-2 py-2 flex flex-col justify-center">
+          <h6 className="text-700 text-sm font-bold">Profession:</h6>
+          <p className="text-700 text-base font-semibold">{userData.occupation ? userData.occupation : 'N/A'}</p>
+        </div>
+        <div className="bg-300 mb-3 w-full rounded-md px-2 py-2 flex flex-col justify-center">
+          <h6 className="text-700 text-sm font-bold">Gender:</h6>
+          <p className="text-700 text-base font-semibold">{userData.gender ? userData.gender : 'N/A'}</p>
+        </div>
+      </div>
+      <div className="bg-300 mb-4 w-full rounded-md px-2 py-2 flex flex-col justify-center">
+        <h6 className="text-700 text-sm font-bold">Address:</h6>
+        <p className="text-700 text-base font-semibold">{userData.address ? userData.address : 'N/A'}</p>
+      </div>
+      <p className="text-base font-normal mb-6">
+        We'll send an email to <p className="font-semibold">{userData.email}</p> to confirm your subscription
+      </p>
+      <div className="flex justify-center gap-3">
+        <button name="closeBtn" className='bg-smallBtn2 py-1.5 px-4 rounded-xl text-700 font-bold' onClick={() => showCard(prevState => !prevState)}>Close</button>
+        <button name="confirmBtn" className='bg-smallBtn py-1.5 px-4 rounded-xl text-700 font-bold' onClick={() => showCard(prevState => !prevState)}>Confirm</button>
       </div>
     </div>
   )
