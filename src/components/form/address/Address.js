6b42234967c19fd1f1ccd55f092c20ac0cda09b1
Andres Marquez <andresdmf55@gmail.com>
2022-12-03 15:01:05 -0500

    Fixed address component

--- src/components/form/address/Address.js
+++ src/components/form/address/Address.js
@@ -1,33 +1,19 @@
 import React from "react";
-import { useState } from "react";
 
 export default function Address({ register }){
-
-  const [count, setCount] = useState(0)
-
-  function handleAddress(event) {
-
-    const { value } = event.target
-
-    if (value.length <= 250) {
-
-      setCount(value.length)
-    } else {
-      setCount(value.length - 1)
-    }
-  } 
 
   return (
     <div className="flex flex-col">
       <label htmlFor="address" className="ml-3 leading-5 sm:ml-4 text-base font-semibold">Address</label>
-      <textarea 
+      <textarea
+        maxLength="250" 
         data-testid="address" 
         id="address"
-        {...register('address')}
-        onChange={handleAddress}
+        {...register('address',{
+          maxLength: 250
+        })}
         className="rounded-xl px-2.5 py-2 my-2 text-sm text-900 bg-input cursor-pointer h-32"
       />
-      <p className="text-xs flex justify-end mr-2">{count}/250</p>
     </div>
   )
 }
